5511e98d0925a7e17df2f031c44f15c31cd6a435
Kristian Popov <kristianpopov03@yahoo.com>
2023-10-19 07:25:30 +0200

    Tausch

---
+++ Informatik/Lektion 1/Tausch/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int x = 5; // x = Integer.parseInt(sc.nextLine());
+        int y = 7; // y = Integer.parseInt(sc.nextLine());
+
+        //Vor dem Tausch
+        //Tausch
+        x = 7;
+        y = 5;
+        
+        // int tausch = x;
+        // x = y
+        // y = tausch;
+
+        //Nach dem Tausch
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(14 + " + " + 5 + " = " + 14 + 5);
+
+
+    }
+}
