c7d5020db3e13e011a906ae73cbe9ceb3324b436
Kristian Popov <kristianpopov03@yahoo.com>
2023-10-19 07:27:25 +0200

    Kreis und Kugel

---
+++ Informatik/Lektion 2/Kreis und Kugel/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int r = Integer.parseInt(sc.nextLine());
+
+        double kreisflach = Math.PI * Math.pow(r,2);
+        double kreisumfang = Math.PI * 2 * r; // leichter zu lesen
+        double volume = (4.0/3.0 * Math.PI * Math.pow(r, 3));
+
+        System.out.println(kreisflach);
+        System.out.println(kreisumfang);
+        System.out.println(volume);
+
+    }
+}
