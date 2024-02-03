08875c45e36b3a3ccb8f3e754522b99e5301a637
Kristian Popov <kristianpopov03@yahoo.com>
2024-02-03 23:05:51 +0100

    Timeline

--- Informatik/Klausur Aufgabe/Prüfung 3/Timeline/src/Timeline.java
+++ Informatik/Klausur Aufgabe/Prüfung 3/Timeline/src/Timeline.java
@@ -2,16 +2,16 @@ public class Timeline
 {
     Post neuePost;
 
-    public void fuegePostEin(Post post)
-    {
-        if(neuePost == null)
-        {
-            neuePost = post;
-        }
-        else
-        {
-            post.vorgaenger = neuePost;
-            neuePost = post;
+   public void fuegePostEin(Post post) {
+        if (neuePost == null) {
+            neuePost = post; // Wenn die Liste leer ist, wird die neue Post zum Kopf
+        } else {
+            Post current = neuePost; // Sonst, erstelle eine temporäre Variable, die auf den Kopf zeigt
+            while (current.vorgaenger != null) { // Durchlaufe die Liste, bis du den letzten Knoten erreichst
+                current = current.vorgaenger;
+            }
+            current.vorgaenger = post; // Füge die neue Post nach dem letzten Knoten ein
+            post.vorgaenger = null; // Setze den Vorgänger der neuen Post auf null, um das Ende der Liste zu zeigen
         }
     }
 
