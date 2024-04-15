b0f2b56a96f1e9a5e7cfa85c034b17bd588ad1cd
Kristian Popov <kristianpopov03@yahoo.com>
2024-04-15 11:55:24 +0200

    Professor - abstract class

--- Programmierung ||/Lektion 15/Person/src/main/java/org/example/Professor.java
+++ Programmierung ||/Lektion 15/Person/src/main/java/org/example/Professor.java
@@ -1,8 +1,12 @@
 package org.example;
 
 public class Professor extends Person{
+    Professor(String fach) {
+        super(fach);
+    }
+
     @Override
-    public String gibTaetigkeitAus(String fach)
+    public String gibTaetigkeitAus()
     {
         return "Der Professor unterrichtet " + fach;
     }
