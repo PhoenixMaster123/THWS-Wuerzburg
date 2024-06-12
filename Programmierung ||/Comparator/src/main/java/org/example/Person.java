cb01297da9fb2d7851b797b23c62f6cfee75312d
Kristian Popov <kristianpopov03@yahoo.com>
2024-06-12 14:16:11 +0200

    Person

--- Programmierung ||/Comparator/src/main/java/org/example/Person.java
+++ Programmierung ||/Comparator/src/main/java/org/example/Person.java
@@ -1,25 +1,25 @@
 package org.example;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
-public class Person implements Comparable<Person>
+public class Person //implements Comparable<Person>
 {
     String vorname;
     String nachname;
+    int alter;
 
-    public Person(String vorname, String nachname)
+    public Person(String vorname, String nachname, int alter)
     {
         this.vorname = vorname;
         this.nachname =nachname;
+        this.alter = alter;
     }
     public String toString()
     {
-        return vorname + " " + nachname;
+        return vorname + " " + nachname + " " + alter;
     }
 
-    @Override
+    /*@Override
     public int compareTo(Person p) {
         if(this.vorname.compareTo(p.vorname) == 0)
         {
@@ -29,5 +29,31 @@ public class Person implements Comparable<Person>
         {
             return this.vorname.compareTo(p.vorname);
         }
+    }
+     */
+    public static void main(String[] args) {
+        List<Person> persons = new ArrayList<>();
+        persons.add(new Person("Terence", "Hill", 27));
+        persons.add(new Person("Bud", "Spencer", 15));
+        Collections.sort(persons, new VornameComparator());
+        System.out.println(persons);
+
+        //
+        Collections.sort(persons, (p1, p2) -> {
+            if (p1.vorname.equals(p2.vorname)) {
+                return p1.nachname.compareTo(p2.nachname);
+            } else {
+                return p1.vorname.compareTo(p2.vorname);
+            }
+        });
+        // shorter
+        /*Collections.sort(persons, (p1, p2)
+                -> p1.vorname.equals(p2.vorname)
+                ? p1.nachname.compareTo(p2.nachname)
+                : p1.vorname.compareTo(p2.vorname));
+        }
+         */
+        List<Person> list = persons.stream().filter(p -> p.alter >= 18).toList(); // we don't need to use collect
+        System.out.println(list);
     }
 }
