770e1ffd1e0a12842f97aa6f4462609beeda7fa8
Kristian Popov <kristianpopov03@yahoo.com>
2024-06-12 10:09:27 +0200

    Delete Programmierung ||/Zusätzliche Aufgaben/Extetions bZV directory

--- Programmierung ||/Zusätzliche Aufgaben/Extetions bZV/GenericReader/src/main/java/org/example/FileContent.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import java.util.List;
-import java.util.Map;
-
-public interface FileContent<T>
-{
-    //Map<String, List<T>> get();
-}
--- Programmierung ||/Zusätzliche Aufgaben/Extetions bZV/GenericReader/src/main/java/org/example/Transform.java
+++
@@ -1,6 +0,0 @@
-package org.example;
-
-public interface Transform<E, T>
-{
-    T transform(E from);
-}
