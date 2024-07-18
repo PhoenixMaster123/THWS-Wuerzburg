4617d79b91f9eb62bbd9d3f1683b3f625310a68d
Kristian Popov <kristianpopov03@yahoo.com>
2024-07-18 20:58:41 +0200

    Main

--- Programmierung ||/Lektion 21/Binaerbaum/src/main/java/org/example/Main.java
+++ Programmierung ||/Lektion 21/Binaerbaum/src/main/java/org/example/Main.java
@@ -3,17 +3,20 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        Binaerbaum baum = new Binaerbaum();
-        Binaerbaum.Knoten k;
-        k = new Binaerbaum.Knoten("Cool");
-        baum.insert(k);
-        k = new Binaerbaum.Knoten("Zylla");
-        baum.insert(k);
-        k = new Binaerbaum.Knoten("Aaronson");
-        baum.insert(k);
-        k = new Binaerbaum.Knoten("Garrett");
-        baum.insert(k);
+        Binaerbaum<String> baum = new Binaerbaum<>();
+        baum.insert(new Binaerbaum.Knoten<>("Cool"));
+        baum.insert(new Binaerbaum.Knoten<>("Zylla"));
+        baum.insert(new Binaerbaum.Knoten<>("Aaronson"));
+        baum.insert(new Binaerbaum.Knoten<>("Garrett"));
+
+        Binaerbaum<Integer> baum2 = new Binaerbaum<>();
+        baum2.insert(new Binaerbaum.Knoten<>(2));
+        baum2.insert(new Binaerbaum.Knoten<>(3));
+        baum2.insert(new Binaerbaum.Knoten<>(6));
+        baum2.insert(new Binaerbaum.Knoten<>(4));
 
         baum.printForward();
+
+        baum2.printForward();
     }
 }
