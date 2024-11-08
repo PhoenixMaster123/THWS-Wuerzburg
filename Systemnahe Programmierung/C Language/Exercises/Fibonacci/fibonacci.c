f1585372d9fc500ce21c695e95c110d86a1bdd92
Kristian Popov <kristianpopov03@yahoo.com>
2024-11-08 20:16:48 +0100

    FibonacciRec

--- Systemnahe Programmierung/C Language/Exercises/Fibonacci/fibonacci.c
+++ Systemnahe Programmierung/C Language/Exercises/Fibonacci/fibonacci.c
@@ -1,18 +1,19 @@
 #include<stdio.h>
 
-int fib(int);
+int fib(int); // We also can implement the logic here
 
 int main()
 {
   int n; // unsigned int n is better, because we want the number to be positive (non-negative) range: 0 to 2^32
   scanf("%d", &n);
 
-  int result = fib(n);
-  printf("%d", result);
+  int result = fib(n); // unsigned long result
+  printf("%d", result); // %ld (long) 
   return 0;
 }
 
-int fib(int n) 
+// recursiv
+int fib(int n)
 {
   if(n == 0 || n == 1)
   {
