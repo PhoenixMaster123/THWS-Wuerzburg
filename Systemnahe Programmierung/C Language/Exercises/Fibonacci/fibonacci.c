54591e0ea6e355b2ce24c39dd8e3531c1f09bf17
Kristian Popov <kristianpopov03@yahoo.com>
2024-11-08 19:46:05 +0100

    Fibonacci

---
+++ Systemnahe Programmierung/C Language/Exercises/Fibonacci/fibonacci.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int fib(int n);
+
+int main()
+{
+  int n;
+  scanf("%d", &n);
+
+  int result = fib(n);
+  printf("%d", result);
+  return 0;
+}
+
+int fib(int n) 
+{
+  if(n == 0 || n == 1)
+  {
+    return n;
+  }
+  return fib(n - 1) + fib(n - 2);
+}
