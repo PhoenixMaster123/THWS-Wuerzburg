959f7f45b033c42222aa3fe38465bf7a20f1055c
Kristian Popov <kristianpopov03@yahoo.com>
2025-01-20 16:32:38 +0100

    Aufgabe 2 (Stream API)

---
+++ Programmierung ||/Klausuren/2024ss/Aufgabe2_Runden/Round.java
@@ -0,0 +1,7 @@
+package Klausurvorberaitung.ss2024.Aufgabe2_Runden;
+
+public class Round{
+    public RoundFunction getRound(double number) {
+        return k -> (int) number;
+    }
+}
---
+++ Programmierung ||/Klausuren/2024ss/Aufgabe2_Runden/RoundFunction.java
@@ -0,0 +1,6 @@
+package Klausurvorberaitung.ss2024.Aufgabe2_Runden;
+
+@FunctionalInterface
+public interface RoundFunction {
+    int round(double number);
+}
